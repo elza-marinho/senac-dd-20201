f9499b2f65510682088777de7fdd1d85b0f896da
elza-marinho <60970734+elza-marinho@users.noreply.github.com>
2020-04-10 21:44:12 -0300

    Add files via upload

---
+++ ClienteBO.java
@@ -0,0 +1,5 @@
+package model.bo.exercicio1;
+
+public class ClienteBO {
+
+}
---
+++ EnderecoBO.java
@@ -0,0 +1,53 @@
+package model.bo.exercicio1;
+
+import model.dao.exercicio1.EnderecoDAO;
+import model.vo.exercicio1.Endereco;
+
+public class EnderecoBO {
+
+	private EnderecoBO bo = new EnderecoBO();
+
+	public String excluir(String textoIdSelecionado) {
+
+		String mensagem = "";
+		try {
+			int idSelecionado = Integer.parseInt(textoIdSelecionado);
+			mensagem = bo.excluir(idSelecionado);
+		} catch (NumberFormatException ex) {
+			mensagem = "Informe um número inteiro";
+		}
+		return mensagem;
+	}
+
+	private EnderecoDAO dao = new EnderecoDAO();
+
+	public String excluir(int idSelecionado) {
+		String mensagem = "";
+		if (dao.temEnderecoCadastradoComId(idSelecionado)) {
+			if (dao.excluir(idSelecionado)) {
+				mensagem = "Excluído com sucesso";
+			} else {
+				mensagem = "Erro ao excluir";
+			}
+		} else {
+			mensagem = "Id informado (" + idSelecionado + ") não existe no banco.";
+		}
+
+		return mensagem;
+	}
+	
+
+	public String salvar(Endereco novoEndereco) {
+		String mensagem = "";
+		novoEndereco = dao.salvar(novoEndereco);
+		if(novoEndereco.getId()>0) {
+			mensagem = "Salvo com sucesso";
+			
+		}else {
+			mensagem = "Erro ao salvar";
+		}
+		return mensagem;
+	}
+	
+
+}
